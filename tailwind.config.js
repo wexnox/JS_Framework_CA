aff16f11c131d60ae579acc5de0b7fecb66fbbec
Lasse R <lasseronnlie@hotmail.com>
2024-03-09 10:38:59 +0100

    Add tailwind.config.js file

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx,vue}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    variants: {},
+    plugins: [],
+  },
+};
+
