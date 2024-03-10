acebff5735267e7618336e19544e823390cfa265
Lasse R <lasseronnlie@hotmail.com>
2024-03-10 09:21:44 +0100

    added tailwind.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
   plugins: [reactRefresh()],
   css: {
     postcss: {
-      plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-      },
-
-    },
-  },
+      plugins: [autoprefixer({}), tailwindcss()],
+    }
+  }
 })
